6b9f913e2cbddf8a0073edbae17515c74284c118
Alihasan Mulji <alihasanmulji@gmail.com>
2025-03-21 20:59:33 +0530

    Added support for custom delimiters

--- stringCalculator.js
+++ stringCalculator.js
@@ -5,13 +5,29 @@ class StringCalculator {
   add(numbersString) {
     if (numbersString === "") return 0;
 
-    const numbers = this._extractDelimiterAndNumbers(numbersString);
-    return numbers.reduce((sum, num) => sum + num, 0);
+    const { delimiter, numbers } =
+      this._extractDelimiterAndNumbers(numbersString);
+    const parsedNumbers = this._parseNumbers(numbers, delimiter);
+
+    return parsedNumbers.reduce((sum, num) => sum + num, 0);
   }
-  _extractDelimiterAndNumbers(numbers) {
-    return numbers
-      .replace(/\n/g, ",")
-      .split(",")
+
+  _extractDelimiterAndNumbers(numbersString) {
+    let delimiter = this.DEFAULT_DELIMITER;
+    let numbers = numbersString;
+
+    if (numbersString.startsWith("//")) {
+      const delimiterEndIndex = numbersString.indexOf("\n");
+      delimiter = numbersString.substring(2, delimiterEndIndex);
+      numbers = numbersString.substring(delimiterEndIndex + 1);
+    }
+    return { delimiter, numbers };
+  }
+
+  _parseNumbers(numbersString, delimiter) {
+    return numbersString
+      .replace(/\n/g, delimiter)
+      .split(delimiter)
       .map((num) => parseInt(num));
   }
 }
