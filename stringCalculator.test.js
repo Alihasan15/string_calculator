76421a621fef1efd8672e08cddee1fc4e5e7f19b
Alihasan Mulji <alihasanmulji@gmail.com>
2025-03-21 20:36:30 +0530

    Added test case for single number input.

--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -6,4 +6,8 @@ describe("StringCalculator", () => {
   test("should return 0 for empty string", () => {
     expect(calculator.add("")).toBe(0);
   });
+
+  test("should return the number for a single number string", () => {
+    expect(calculator.add("1")).toBe(1);
+  });
 });
